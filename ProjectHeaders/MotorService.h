d4ae62566913ad0f17dd4637560b6debe911eb30
soychoi <soychoi@stanford.edu>
2026-02-01 18:41:00 +0000

    MotorService.h edited for full functionality.

--- ProjectHeaders/MotorService.h
+++ ProjectHeaders/MotorService.h
@@ -1,43 +1,37 @@
-/****************************************************************************
-
-  Header file for template service
-  based on the Gen 2 Events and Services Framework
-
- ****************************************************************************/
-
 #ifndef MOTOR_SERVICE_H
 #define MOTOR_SERVICE_H
 
 #include <stdint.h>
 #include <stdbool.h>
-
 #include "ES_Configure.h"
 #include "ES_Framework.h"
 
-/* Public interface required by ES_Framework */
+/*
+  MotorService is the brain for motion.
+  Other services post events; MotorService decides what the motors should do.
+
+  Motion EventParam formats (I keep these simple):
+    ES_TRANSLATE:
+      bit0: speed   (0=half, 1=full)
+      bit1: dir     (0=fwd,  1=rev)
+
+    ES_ROTATE:
+      bit0: angle   (0=45,   1=90)
+      bit1: dir     (0=CW,   1=CCW)
+*/
+
 bool InitMotorService(uint8_t Priority);
 bool PostMotorService(ES_Event_t ThisEvent);
 ES_Event_t RunMotorService(ES_Event_t ThisEvent);
 
-/* ----------------- Command Param Packing -----------------
-   MotorService expects motion commands to arrive as ES events.
-
-   ES_TRANSLATE EventParam encoding:
-     bit0: speed   (0=half, 1=full)
-     bit1: dir     (0=fwd,  1=rev)
-
-   ES_ROTATE EventParam encoding:
-     bit0: angle   (0=45,   1=90)
-     bit1: dir     (0=CW,   1=CCW)
------------------------------------------------------------ */
-
+/* Translate param helpers */
 typedef enum { TRANS_HALF = 0, TRANS_FULL = 1 } TransSpeed_t;
 typedef enum { DIR_FWD = 0, DIR_REV = 1 } TransDir_t;
 
+/* Rotate param helpers */
 typedef enum { ROT_45 = 0, ROT_90 = 1 } RotAngle_t;
 typedef enum { ROT_CW = 0, ROT_CCW = 1 } RotDir_t;
 
-/* helper pack/unpack */
 static inline uint16_t PackTranslateParam(TransSpeed_t spd, TransDir_t dir)
 {
   return (uint16_t)((spd & 0x1u) | ((dir & 0x1u) << 1));
@@ -61,4 +55,3 @@ static inline void UnpackRotateParam(uint16_t p, RotAngle_t *ang, RotDir_t *dir)
 }
 
 #endif
-
