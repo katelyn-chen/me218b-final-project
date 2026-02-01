d652d4aae5fe7026c402e4efa581d47317797035
soychoi <soychoi@stanford.edu>
2026-02-01 19:07:03 +0000

    SPIService.h edited

--- ProjectHeaders/SPIService.h
+++ ProjectHeaders/SPIService.h
@@ -1,17 +1,16 @@
-#ifndef SPIService_H
-#define SPIService_H
+#ifndef SPI_SERVICE_H
+#define SPI_SERVICE_H
 
-#include "ES_Types.h"
 #include <stdint.h>
 #include <stdbool.h>
 
+#include "ES_Configure.h"
+#include "ES_Framework.h"
 #include "ES_Events.h"
-#include "ES_Port.h"  
 
-// Public Function Prototypes
-
+/* Public Function Prototypes */
 bool InitSPIService(uint8_t Priority);
 bool PostSPIService(ES_Event_t ThisEvent);
 ES_Event_t RunSPIService(ES_Event_t ThisEvent);
 
-#endif /* SPIService_H */
+#endif /* SPI_SERVICE_H */
