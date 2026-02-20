e1961f4b2df26ff1aaec2bfcb2f0f71d35466c01
soychoi <soychoi@stanford.edu>
2026-02-20 01:08:38 +0000

    Editing to post L/R

--- PICFollower/ProjectHeaders/BeaconService.h
+++ PICFollower/ProjectHeaders/BeaconService.h
@@ -1,20 +1,44 @@
 #ifndef BEACON_SERVICE_H
 #define BEACON_SERVICE_H
 
-//#include <stdint.h>
-//#include <stdbool.h>
-//#include "ES_Configure.h"
-////#include "ES_Framework.h"
-//#include "ES_Types.h"     /* gets bool type for returns */
-
 #include "ES_Configure.h"
 #include "ES_Types.h"
 #include "ES_Events.h"
 
 /*
   BeaconService watches the IR beacon signal and decides “beacon found”.
-  I’m keeping it event-only: this service posts ES_BEACON_FOUND and that’s it.
+  This service posts ES_BEACON_FOUND with EventParam encoding:
+    - Beacon ID (G/B/R/L)
+    - Bucket side (LEFT/RIGHT/UNKNOWN)
 */
+
+/* beacon IDs from the Seesaw table */
+typedef enum {
+  BEACON_ID_NONE = 0,
+  BEACON_ID_G    = 1,   /* 3333 Hz */
+  BEACON_ID_B    = 2,   /* 1427 Hz */
+  BEACON_ID_R    = 3,   /*  909 Hz */
+  BEACON_ID_L    = 4    /* 2000 Hz */
+} BeaconId_t;
+
+/* bucket side classification */
+typedef enum {
+  BEACON_SIDE_UNKNOWN = 0,
+  BEACON_SIDE_LEFT    = 1,
+  BEACON_SIDE_RIGHT   = 2
+} BeaconSide_t;
+
+/* Pack into uint16_t: [15:8] BeaconId, [7:0] BeaconSide */
+static inline uint16_t PackBeaconParam(BeaconId_t id, BeaconSide_t side)
+{
+  return (uint16_t)(((uint16_t)id << 8) | ((uint16_t)side & 0xFFu));
+}
+
+static inline void UnpackBeaconParam(uint16_t param, BeaconId_t *id, BeaconSide_t *side)
+{
+  if (id)   *id   = (BeaconId_t)((param >> 8) & 0xFFu);
+  if (side) *side = (BeaconSide_t)(param & 0xFFu);
+}
 
 bool InitBeaconService(uint8_t Priority);
 bool PostBeaconService(ES_Event_t ThisEvent);
