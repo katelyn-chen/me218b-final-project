f3ecbfacac2c7f1318120840ecd7f35da780398a
Katelyn E. Chen <katelync@stanford.edu>
2026-03-04 16:57:17 -0800

    tune collect and dispense servos

--- PICLeader/ProjectSource/CollectService.c
+++ PICLeader/ProjectSource/CollectService.c
@@ -56,7 +56,7 @@
 /* Arm (OC4 -> RA4): ready/down */
 #define ARM_UP_TICKS           1530u // tested!!! originally 1540 DONT CHANGE VALUES
 #define ARM_DOWN_TICKS         3330u
-#define ARM_MIDDLE_TICKS       1750u
+#define ARM_MIDDLE_TICKS       1950u
 // 1540, 3300 is 90 degree 
 
 #define BUCKET_ARM_DISPENSE_TICKS    3000u 
